51719dc2917300661e960fca4cf9f18b7432cf60
Anthony <anthonyloor462@gmail.com>
2022-12-30 11:28:15 -0500

    Cargar plantilla y formulario

--- public/js/main.js
+++ public/js/main.js
@@ -1,1 +1,41 @@
-console.log('Hola Mundo')
+const loadInitialTemplate = () => {
+    const template = `
+    <div class="container p-5">
+        <div class="card">
+            <div class="card-body">
+                <h1>Administracion de Usuarios</h1>
+                <form id="user-form">
+                    <div class="mb-3">
+                      <label class="form-label">Nombre</label>
+                      <input type="text" name="name" class="form-control">
+                    </div>
+                    <div class="mb-3">
+                        <label class="form-label">Apellidos</label>
+                        <input type="text" name="lastname" class="form-control">
+                      </div>
+                    <button type="submit" class="btn btn-primary">Enviar</button>
+                  </form>
+            </div>
+        </div>
+        <ul id="user-list"></ul>
+    </div>
+    `
+
+    const body = document.getElementsByTagName('body')[0]
+    body.innerHTML = template
+}
+
+const addFormListener = () => {
+    const userForm = document.getElementById('user-form')
+    userForm.onsubmit = async (e) => {
+        e.preventDefault()
+        const formData = new FormData(userForm)
+        const data = Object.fromEntries(formData.entries())
+        console.log(data)
+    }
+}
+
+window.onload = () => {
+    loadInitialTemplate()
+    addFormListener()
+}
