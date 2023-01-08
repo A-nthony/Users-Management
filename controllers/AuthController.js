caeee44ce9827fd5e13aafc23f216c3e05ebb8c5
Anthony <anthonyloor462@gmail.com>
2023-01-08 17:24:31 -0500

    Middleware de validacion del JWT

--- controllers/AuthController.js
+++ controllers/AuthController.js
@@ -1,21 +1,39 @@
-const Users = require('../models/User')
+const express = require('express')
 const bcrypt = require('bcrypt')
-const jwt = require('jsonwebtoken')
 const expressJwt = require('express-jwt')
+const jwt = require('jsonwebtoken')
+const User = require('../models/User')
 
-const signToken = _id => jwt.sign({ _id }, 'mi-string-secreto')
+const validateJWT = expressJwt({ secret: process.env.SECRET, algorithms: ['HS256']})
+
+const signToken = _id => jwt.sign({ _id }, process.env.SECRET)
+
+const findAndAssignUser = async (req, res, next) => {
+    try {
+        const user = await User.findById(req.user._id)
+        if (!user) {
+            return res.status(401).end()
+        }
+        req.user = user
+        next()
+    } catch (error) {
+        next(error)
+    }
+}
+
+const isAuthenticated = express.Router().use(validateJWT, findAndAssignUser)
 
 const Auth = {
     register: async (req, res) => {
         const { body } = req
         try {
-            const isUser = await Users.findOne({ email: body.email})
+            const isUser = await User.findOne({ email: body.email})
             if (isUser) {
                 return res.status(403).send('Usuario ya existe')
             }
             const salt = await bcrypt.genSalt()
             const hashed = await bcrypt.hash(body.password, salt)
-            const user = Users.create({name: body.name, lastname: body.lastname, email: body.email, password: hashed, salt})
+            const user = User.create({name: body.name, lastname: body.lastname, email: body.email, password: hashed, salt})
             const signed = signToken(user._id)
             res.status(201).send(signed)
         } catch (err) {
@@ -27,7 +45,7 @@ const Auth = {
     login: async (req, res) => {
         const { body } = req
         try {
-            const user = await Users.findOne({ email: body.email})
+            const user = await User.findOne({ email: body.email})
             if (!user) {
                 res.status(403).send('Usuario y/o contraseña inválida')
             } else {
@@ -48,4 +66,4 @@ const Auth = {
 }
 
 
-module.exports = Auth
+module.exports = { isAuthenticated, Auth}
